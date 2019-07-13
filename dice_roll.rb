1c18a97557fa87935927df6a6ac0547ef7e1446d
Randall Taylor <randyisbusy@gmail.com>
2019-07-13 20:27:21 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,8 +3,9 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  y = [0..10]
+  y = [0, 1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10]
   x = rand(0..10)
-  puts y
+  dice = y[x]
+  puts dice
 end
 roll
